56d2dc3f1555cd50871471893c4ded09d1cc3b9d
Marc <teste74@hotmail.fr>
2024-07-13 18:31:24 +0200

    Update mysql_secure.sh

--- Build/Ressources/Packages/MariaDB/mysql_secure.sh
+++ Build/Ressources/Packages/MariaDB/mysql_secure.sh
@@ -1,4 +1,4 @@
 ####################################################################################################################################################################################
 # Securisation de la base de donnee #
 #####################################
-(echo ""; echo "y"; echo "y"; echo "$PASS_ROOT_SQL"; echo "$PASS_ROOT_SQL"; echo "y"; echo "y"; echo "y"; echo "y") | mysql_secure_installation | 1>/dev/null;
+(echo ""; echo "y"; echo "y"; echo "$PASS_ROOT_SQL"; echo "$PASS_ROOT_SQL"; echo "y"; echo "y"; echo "y"; echo "y") | mysql_secure_installation;
