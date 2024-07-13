d60bb0c2752604792412c9601931ee8edbd7aadb
Marc <teste74@hotmail.fr>
2024-07-13 20:31:43 +0200

    Create start.sh

---
+++ Build/Ressources/Packages/Web/glpi/start.sh
@@ -0,0 +1,14 @@
+#######################################################################################################################################
+# Installation du site #
+########################
+/var/www/html/glpi/bin/console db:install \
+--reconfigure \
+--default-language=$LANGUE \
+--db-host=$HOST \
+--db-port=3306 \
+--db-name=$DATABSE \
+--db-user=$USERNAMEDB \
+--db-password=$PASSDB \
+--force;
+
+
