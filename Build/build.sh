138ae89a9f85d7d18a696ff80dd51ca99ac09478
Marc <teste74@hotmail.fr>
2024-07-13 12:03:30 +0200

    Update build.sh

--- Build/build.sh
+++ Build/build.sh
@@ -2,7 +2,7 @@
 #####################################################################################
 # Auteur        : Marc Jaffre
 # Version       : 1.0 (Alpha)
-# Utilisation   : bash build.sh <Dockerfile> <image_de_sortie>
+# Utilisation   : bash build.sh <Dossier> <DockerFile> <Image_de_sortie>
 # Presentation  :
 # - Editer le fichier Dockerfile
 # - Supprimer l-image
@@ -17,15 +17,14 @@ clear;
 #####################################################################################
 # Editer le DockerFile #
 ########################
-nano ./Dockerfile/$1;
+nano ./Dockerfile/$2;
 
 #####################################################################################
 # Supprimer l-image #
 ####################
-docker image rm -f $2 2>/dev/null;
+docker image rm -f $3 2>/dev/null;
 
 #####################################################################################
 # Construction de l-image #
 ###########################
-docker build -f ./Dockerfile/$1 . -t $2;
-#####################################################################################
+docker build -f ./$1/$2 . -t $3
