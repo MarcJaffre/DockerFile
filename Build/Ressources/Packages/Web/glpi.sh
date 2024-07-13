832f6fa1bffc3ebd5ae208d17f970a41502124e6
Marc <teste74@hotmail.fr>
2024-07-13 20:09:47 +0200

    Update glpi.sh

--- Build/Ressources/Packages/Web/glpi.sh
+++ Build/Ressources/Packages/Web/glpi.sh
@@ -44,12 +44,12 @@ EOF
 # Deploiement de GLPI #
 #######################
 # Telecharger GLPI
-wget https://github.com/glpi-project/glpi/releases/download/$VERSION/glpi-$VERSION.tgz -O /tmp/glpi.tgz 2>/dev/null;
+#wget https://github.com/glpi-project/glpi/releases/download/$VERSION/glpi-$VERSION.tgz -O /tmp/glpi.tgz 2>/dev/null;
 
 # Extraire le fichier compresser
-tar -xf /tmp/glpi.tgz -C /var/www/html;
+#tar -xf /tmp/glpi.tgz -C /var/www/html;
 
 # Permissions (Apache)
-chown -R $USERNAME:$GROUP /var/www/html;
-chmod 755 /var/www/html/glpi;
+#chown -R $USERNAME:$GROUP /var/www/html;
+#chmod 755 /var/www/html/glpi;
 
