65b4628153cf82a8aefe79977d83f3c61a1b220f
Marc <teste74@hotmail.fr>
2024-07-13 19:40:49 +0200

    Create glpi.sh

---
+++ Build/Ressources/Packages/Web/glpi.sh
@@ -0,0 +1,9 @@
+# Telecharger GLPI
+wget https://github.com/glpi-project/glpi/releases/download/$VERSION/glpi-$VERSION.tgz -O /tmp/glpi.tgz 2>/dev/null;
+
+# Extraire le fichier compresser
+tar -xf /tmp/glpi.tgz -C /var/www/html;
+
+# Permissions (Apache)
+chown -R www-data:www-data /var/www/html;
+chmod 755 /var/www/html/glpi;
