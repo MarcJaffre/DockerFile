766ad5c40ccfb3c27c22296f4dcd99eea6196a7e
Marc <teste74@hotmail.fr>
2024-07-13 18:44:39 +0200

    Update install.sh

--- Build/Ressources/Packages/MariaDB/install.sh
+++ Build/Ressources/Packages/MariaDB/install.sh
@@ -3,3 +3,9 @@
 ###########################
 apt install -y mariadb-server 1>/dev/null;
 sleep 5;
+
+echo "[mysqld]
+datadir      = /var/lib/mysql
+bind-address = 0.0.0.0
+port         = 3306
+skip-grant-tables" > /etc/mysql/my.cnf
