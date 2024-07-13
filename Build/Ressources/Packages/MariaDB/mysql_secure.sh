4c21c0082e30989295bf488abc8a885573478dca
Marc <teste74@hotmail.fr>
2024-07-13 19:30:11 +0200

    Update mysql_secure.sh

--- Build/Ressources/Packages/MariaDB/mysql_secure.sh
+++ Build/Ressources/Packages/MariaDB/mysql_secure.sh
@@ -3,5 +3,3 @@
 #####################################
 (echo ""; echo "y"; echo "y"; echo "$PASS_ROOT_SQL"; echo "$PASS_ROOT_SQL"; echo "y"; echo "y"; echo "y"; echo "y") | /usr/bin/mysql_secure_installation --skip-grant-tables;
 
-mkdir -p /var/lib/mysql /var/run/mysqld;
-chown -R $USERNAME:$GROUP /var/lib/mysql /var/run/mysqld /var/run/mysqld;
