6c7379bee64d5a1f7e03c3577e86a16d91323127
Marc <teste74@hotmail.fr>
2024-07-13 17:43:25 +0200

    Update apache2.sh

--- Build/Ressources/Packages/Apache2/apache2.sh
+++ Build/Ressources/Packages/Apache2/apache2.sh
@@ -1,8 +1,8 @@
 ##################################################################################################
 # Installation du paquet Apache2 #
 ##################################
-#apt install -y apache2;
-#apt install -y apache2-utils;
-#echo "ServerName localhost" >> /etc/apache2/apache2.conf
-#mkdir -p /var/lock/apache2 /var/log/apache2 /var/run/apache2
-#chown -R $USERNAME:$GROUP /var/lock/apache2 /var/log/apache2 /var/run/apache2 /var/www/html
+apt install -y apache2;
+apt install -y apache2-utils;
+echo "ServerName localhost" >> /etc/apache2/apache2.conf
+mkdir -p /var/lock/apache2 /var/log/apache2 /var/run/apache2
+chown -R $USERNAME:$GROUP /var/lock/apache2 /var/log/apache2 /var/run/apache2 /var/www/html
