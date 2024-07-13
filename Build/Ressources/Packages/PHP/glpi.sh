04196e75db36e29dbb8aa01705cbdf86b1c8d053
Marc <teste74@hotmail.fr>
2024-07-13 19:40:28 +0200

    Create glpi.sh

---
+++ Build/Ressources/Packages/PHP/glpi.sh
@@ -0,0 +1,13 @@
+##############################################################################################################################
+# Modules PHP pour GLPI #
+#########################
+apt install -y php-curl;
+apt install -y php-gd;
+apt install -y php-intl;
+apt install -y php-mysqli;
+apt install -y php-simplexml 
+apt install -y php-bz2;
+apt install -y php-ldap;
+apt install -y php-mbstring;
+apt install -y php-symfony-polyfill-ctype;
+apt install -y php-zip;
