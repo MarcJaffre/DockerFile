ec80cc290467e547d48e45e19891a050d184b8db
Marc <teste74@hotmail.fr>
2024-07-13 20:04:48 +0200

    Update glpi.sh

--- Build/Ressources/Packages/Web/glpi.sh
+++ Build/Ressources/Packages/Web/glpi.sh
@@ -7,3 +7,35 @@ tar -xf /tmp/glpi.tgz -C /var/www/html;
 # Permissions (Apache)
 chown -R $USERNAME:$GROUP /var/www/html;
 chmod 755 /var/www/html/glpi;
+
+
+
+cat > /etc/apache2/sites-available/000-default.conf << EOF
+<VirtualHost *:80>
+ # Nom du serveur (/etc/hosts)
+ ServerName debian.lan
+
+ # Dossier Web Public
+ DocumentRoot /var/www/html/glpi/public
+        
+ # Fichier à charger par défaut (ordre)
+ <IfModule dir_module>
+   DirectoryIndex index.php index.html
+ </IfModule>
+
+ # Alias
+ Alias "/glpi" "/var/www/html/glpi/public"
+
+ # Log
+ ErrorLog ${APACHE_LOG_DIR}/error.log
+ CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+ # Repertoire
+ <Directory /var/www/html/glpi/public>
+   Require all granted
+   RewriteEngine On
+   RewriteCond %{REQUEST_FILENAME} !-f
+   RewriteRule ^(.*)$ index.php [QSA,L]
+ </Directory>
+</VirtualHost>
+EOF
