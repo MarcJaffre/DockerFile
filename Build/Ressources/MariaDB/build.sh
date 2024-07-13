9a7f83cfc30ca5ee359986cd494283a1b40749c3
Marc <teste74@hotmail.fr>
2024-07-13 16:56:28 +0200

    Update build.sh

--- Build/Ressources/MariaDB/build.sh
+++ Build/Ressources/MariaDB/build.sh
@@ -4,3 +4,4 @@
 # Configuration de MariaDB #
 ############################
 sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf;
+sed -i -e"s/^# port = 3306/port = 3306/" /etc/mysql/my.cnf;
