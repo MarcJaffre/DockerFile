92560c1ba2a2338c4657172c884a089e3ebb1f8a
Marc <teste74@hotmail.fr>
2024-07-13 17:25:19 +0200

    Create apache2.sh

---
+++ Build/Ressources/Packages/apache2.sh
@@ -0,0 +1,5 @@
+######################################################################
+# Installation du paquet Apache2 #
+##################################
+apt install -y apache2;
+apt install -y apache2-utils;
