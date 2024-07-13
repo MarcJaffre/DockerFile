2ac3b2ccb6130f45896a6de5cba54f2231139426
Marc <teste74@hotmail.fr>
2024-07-13 20:47:45 +0200

    Update start.sh

--- Build/Ressources/Packages/Web/glpi/start.sh
+++ Build/Ressources/Packages/Web/glpi/start.sh
@@ -14,6 +14,13 @@ echo "yes no" | /var/www/html/glpi/bin/console db:install \
 --force;
 
 #######################################################################################################################################
+# Verification #
+################
+# if [ $(curl http://localhost) = XXX ]; then
+# rm /var/www/html/glpi/install.install.php:
+# fi
+
+#######################################################################################################################################
 # Demarrage du conteneur #
 ##########################
 /usr/sbin/apache2ctl -D FOREGROUND;
